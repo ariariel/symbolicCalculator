d8eacc93c4d3dee45f0bef3472397b88fd094b38
Marie Urmano <urmanom@ufl.edu>
2014-04-11 15:37:41 -0400

    Changed a couple things based on Integer class
    Still don't know how we are going to change Input to int, though. This
    is going to be ideal if we want our calculator to work at all o.o

--- Operator.cpp
+++ Operator.cpp
@@ -1,4 +1,5 @@
 #include "Operator.h"
+#include "Integer.h"
 
 
 Operator::Operator(){
@@ -15,14 +16,17 @@ char Operator::getOperand(){
 
 void Operator::add(int index, vector<Input> &RPNVec){
 	int result;
-	if((typeid(RPNVec[index]) == typeid(int)) && (typeid(RPNVec[index + 1]) == typeid(int))){
+	if((typeid(RPNVec[index]) == typeid(Integer)) && (typeid(RPNVec[index + 1]) == typeid(Integer))){
 		int number1 = RPNVec[index]; //Error here: no suitable conversion from "Input" to "int" exists
 		//do we need another conversion method that changes to int?
 		int number2 = RPNVec[index + 1];
 		result = number1 + number2;
+		RPNVec[index] = result;
 	}
 	else{
-		RPNVec
+		 Input input = RPNVec[index];
+		 Input input2 = RPNVec[index + 1];
+		 //how to combine two Input values and store into the same index?
 	}
 	RPNVec.erase(RPNVec.begin() + 1);
 	//delete second, third elements
