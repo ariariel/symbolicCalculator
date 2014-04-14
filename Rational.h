d188b9282350a4eb01201af0878fa1770af037a1
Ari Farias <ari.ariel.farias@gmail.com>
2014-04-14 17:43:06 -0400

    Fixed Rational.h

--- Rational.h
+++ Rational.h
@@ -3,6 +3,7 @@
 #ifndef RATIONAL_H_
 #define RATIONAL_H_
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
@@ -12,30 +13,41 @@ class Rational: public Input
 {
 private:
 
-  int Numerator_;
-  int Denominator_;
+  int Numerator;
+  int Denominator;
 
 public:
 
   // Construct rational from numerator and denominator
   Rational( int num, int den )
   {
-     this->Numerator_=num;
+     this->Numerator=num;
 
-    if (Denominator_ == 0)
+    if (Denominator == 0)
     {
-      cout<< "cannot have 0 as a demoninator";
+      throw overflow_error("cannot have 0 as a demoninator");
     }
     else
     {
-      this->Denominator_ = den;
+      this->Denominator = den;
     }
   }
-
+  int getNoom(){
+    return Numerator;
+  }
+  void setNoom(int num){
+    this->Numerator = num;
+  }
+  int getDen(){
+    return Denominator;
+  }
+  void setDen(int den){
+    this->Denominator = den;
+  }
   int GCD()
   {
-          int A = Numerator_;
-          int B = Denominator_;
+          int A = Numerator;
+          int B = Denominator;
 
           int Temp;
 
@@ -55,12 +67,16 @@ public:
 
           if(GCDNumber != 0)
           {
-                  Numerator_ = Numerator_ / GCDNumber;
-                  Denominator_ = Denominator_ / GCDNumber;
+                  Numerator = Numerator / GCDNumber;
+                  Denominator = Denominator / GCDNumber;
           }
           else{
             cout<< ""<< endl;
           }
+  }
+
+  Rational add(Rational rat){
+
   }
 
 };
