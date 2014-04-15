557a3346fc24c9db8261ff69f2eeea4ddf12d948
Marie Urmano <urmanom@ufl.edu>
2014-04-14 21:22:45 -0400

    Rational.h with mult, divide

--- Rational.h
+++ Rational.h
@@ -4,6 +4,7 @@
 #define RATIONAL_H_
 #include <iostream>
 #include <stdexcept>
+#include "Input.h"
 
 using namespace std;
 
@@ -19,7 +20,7 @@ private:
 public:
 
   // Construct rational from numerator and denominator
-  Rational( int num, int den )
+  Rational(int num, int den)
   {
      this->Numerator=num;
 
@@ -44,6 +45,33 @@ public:
   void setDen(int den){
     this->Denominator = den;
   }
+
+  //Multiply method for rational
+  Rational multiply(Rational* rat){
+	    int Numerator1 = this->Numerator;
+		int Denominator1 = this->Denominator;
+		int Numerator2 = rat->getNoom();
+		int Denominator2 = rat->getDen();
+		int finalNumerator = Numerator1 * Numerator2;
+		int finalDenominator = Denominator1 * Denominator2;
+		Rational finalRat = Rational(finalNumerator, finalDenominator);
+		finalRat.Simplify();
+		return finalRat;
+  }
+
+  //Divide method for rational
+  Rational divide(Rational* rat){
+		int Numerator1 = this->Numerator;
+		int Denominator1 = this->Denominator;
+		int Numerator2 = rat->getNoom();
+		int Denominator2 = rat->getDen();
+		int finalNumerator = Numerator1 * Denominator2;
+		int finalDenominator = Denominator1 * Numerator2;
+		Rational finalRat = Rational(finalNumerator, finalDenominator);
+		finalRat.Simplify();
+		return finalRat;
+  }
+
   int GCD()
   {
           int A = Numerator;
@@ -61,24 +89,17 @@ public:
           return A;
   }
   //Simplify the rational number to its smallest form
-  void Simplify()
-  {
-          int GCDNumber = GCD();
-
+  void Simplify(){ 
+	  int GCDNumber = GCD();
           if(GCDNumber != 0)
           {
                   Numerator = Numerator / GCDNumber;
                   Denominator = Denominator / GCDNumber;
           }
           else{
-            cout<< ""<< endl;
+            cout<< "" << endl;
           }
   }
-
-  Rational add(Rational rat){
-
-  }
-
 };
 #endif 
 
