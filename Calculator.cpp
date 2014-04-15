a7ae815b5976ce65f90e65132b5f386944d3fae6
Ari Farias <ari.ariel.farias@gmail.com>
2014-04-14 22:44:50 -0400

    Updated Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,4 +1,5 @@
 #include "Calculator.h"
+#include <string>
 using namespace std;
 
 Calculator::Calculator(){
@@ -17,9 +18,11 @@ void Calculator::setVec(vector<Input*> Vec){
 	RPNVec = Vec;
 }
 vector<Input*> Calculator::rewriteVec(int index, Input* res){
+	//Rational* res1 = (Rational*)res;
+	//cout<< typeid(*res).name()<<endl;
 	vector<Input*> result;
 	for(int i =0; i<index; i++){
-		result.push_back(RPNVec[i+2]);
+		result.push_back(RPNVec[i]);
 	}
 	result.push_back(res);
 	for(int i =index+1; i<RPNVec.size()-2; i++){
@@ -27,7 +30,28 @@ vector<Input*> Calculator::rewriteVec(int index, Input* res){
 	}
 	return result;
 }
-
+void Calculator::ansParse(string& y) {
+    size_t check = y.find("Ans");
+    size_t check2 = y.find("ans");
+    size_t position = 0;
+    if(check != string::npos) {
+        position = check;
+    }
+    else if(check2 != string::npos) {
+        position = check2;
+    }
+	while(position != string::npos){
+    stringstream ss;
+    for(int i = 0; i < position; i++) {
+        ss << y.at(i);
+    }
+    ss << getlastAns();
+    for(int i = position + 3; i< y.size(); i++) {
+        ss << y.at(i);
+    }
+    y = ss.str();
+	}
+}
 
 // string Calculator::addAllToIndex(Input input1, Input input2, Operator operate, vector<Input> &RPNVec){
 // 	ostringstream str;
@@ -60,6 +84,7 @@ vector<Input*> Calculator::rewriteVec(int index, Input* res){
 
 void Calculator::add(int index){
 	int result;
+	
 	if((typeid(*RPNVec[index]) == typeid(Integer)) && (typeid(*RPNVec[index + 1]) == typeid(Integer))){
 		Integer* first = (Integer*)RPNVec[index];
 		int number1 = first->getInteger();
@@ -67,6 +92,17 @@ void Calculator::add(int index){
 		int number2 = second->getInteger();
 		result = number1 + number2;
 		Integer* res = new Integer(result);
+		RPNVec = rewriteVec(index, res);
+	}
+	// string one = typeid(*RPNVec[index]).name();
+	// string two = typeid(*RPNVec[index + 1]).name();
+	else if((typeid(*RPNVec[index]) == typeid(Rational)) && (typeid(*RPNVec[index + 1]) == typeid(Rational))){
+		// cout<< typeid(*RPNVec[index]).name()<<endl;
+		// cout<< typeid(*RPNVec[index+1]).name()<<endl;
+		Rational* first = (Rational*)RPNVec[index]; ///BUG RIGHT HERE
+		Rational* second = (Rational*)RPNVec[index + 1];
+		Rational* res = first->add(second);
+		res->Simplify();
 		RPNVec = rewriteVec(index, res);
 	}
 	// else{ 
@@ -96,6 +132,13 @@ void Calculator::subtract(int index){
 		Integer* res = new Integer(result);
 		RPNVec = rewriteVec(index,res);
 	}
+	else if((typeid(*RPNVec[index]) == typeid(Rational)) && (typeid(*RPNVec[index + 1]) == typeid(Rational))){
+		Rational* ratfirst = (Rational*)RPNVec[index]; ///BUG RIGHT HERE
+		Rational* ratsecond = (Rational*)RPNVec[index + 1];
+		Rational* res = ratfirst->subtract(ratsecond);
+		res->Simplify();
+		RPNVec = rewriteVec(index, res);
+	}
 }
 void Calculator::divide(int index){
 	int result;
@@ -111,16 +154,25 @@ void Calculator::divide(int index){
 		else if (number2 != 0){
 			result = number1 / number2;
 		}
-		if(typeid(result)==typeid(1)){
+		if(number1%number2==0){
 			Integer* res = new Integer(result);
 			RPNVec = rewriteVec(index,res);
 		}
-		else if(typeid(result)==typeid(0.5)){
-			Rational* res = new Rational(number1, number2);
-			res->Simplify();
-			RPNVec = rewriteVec(index, res);
+		else{
+			Rational* rat = new Rational(number1, number2);
+			rat->Simplify();
+			//res->Simplify();
+			RPNVec = rewriteVec(index, rat);
+			
 
 		}
+	}
+	else if((typeid(*RPNVec[index]) == typeid(Rational)) && (typeid(*RPNVec[index + 1]) == typeid(Rational))){
+		Rational* ratfirst = (Rational*)RPNVec[index]; ///BUG RIGHT HERE
+		Rational* ratsecond = (Rational*)RPNVec[index + 1];
+		Rational* res = ratfirst->divide(ratsecond);
+		res->Simplify();
+		RPNVec = rewriteVec(index, res);
 	}
 }
 void Calculator::multiply(int index){
@@ -133,6 +185,13 @@ void Calculator::multiply(int index){
 		result = number1 * number2;
 		Integer* res = new Integer(result);
 		RPNVec = rewriteVec(index,res);
+	}
+	else if((typeid(*RPNVec[index]) == typeid(Rational)) && (typeid(*RPNVec[index + 1]) == typeid(Rational))){
+		Rational* ratfirst = (Rational*)RPNVec[index]; ///BUG RIGHT HERE
+		Rational* ratsecond = (Rational*)RPNVec[index + 1];
+		Rational* res = ratfirst->multiply(ratsecond);
+		res->Simplify();
+		RPNVec = rewriteVec(index, res);
 	}
 }
 void Calculator::power(int index){
@@ -160,9 +219,18 @@ int Calculator::ipow(int base, int exp){
 
     return result;
 }
+void Calculator::printSolution(){
+	if(typeid(*RPNVec[0]) == typeid(Integer)){
+		Integer* num = (Integer*)RPNVec[0];
+		cout << num->getInteger() << endl;
+	}
+	else if(typeid(*RPNVec[0]) == typeid(Rational)){
+		Rational* rat = (Rational*)RPNVec[0];
+		cout << rat->getNoom() << "/" << rat->getDen() << endl;
+	}
+}
 
 bool Calculator:: solve(){ 
-
 	for(int i = 0; i<RPNVec.size(); i++){
 		if(typeid(*RPNVec[i]) == typeid(Operator)){
 			Operator* opObject = (Operator*)RPNVec[i];
@@ -182,11 +250,19 @@ bool Calculator:: solve(){
 		}
 	}
 	if(RPNVec.size() == 1){
-
-			Integer* b = (Integer*)RPNVec[0];
-			int num = b->getInteger();
-			cout << num << endl;
-			cout<< "SOLVED!" << endl;
+			if(typeid(*RPNVec[0])== typeid(Rational)){
+				Rational* rat = (Rational*)RPNVec[0];
+				ostringstream ss;
+				ss<<rat->getNoom() <<"/"<< rat->getDen();
+				setAns(ss.str());
+			}
+			else if(typeid(*RPNVec[0])== typeid(Integer)){
+				Integer* ans = (Integer*)RPNVec[0];
+				ostringstream ss;
+				ss<<ans->getInteger();
+				setAns(ss.str());
+			}
+			printSolution();
 			return true;
 	}
 }
